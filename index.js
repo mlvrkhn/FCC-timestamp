3f0ba5d4ba556af4f8698da3e7c19a77008c4266
mlvrkhn <martin.guide@gmail.com>
2024-02-05 13:59:13 +0100

    Add moment.js library and update API endpoint

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const moment = require('moment');
+
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -23,20 +25,37 @@ app.get('/api/hello', function (req, res) {
 	res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/:date?', function (req, res) {
-	var input = req.params.date;
-	var date;
+app.get('/api/:unix', function (req, res) {
+	const { unix } = req.params;
 
-	if (!isNaN(input)) {
-		date = new Date(parseInt(input));
+	if (!isNaN(unix)) {
+		const date = new Date(parseInt(unix));
+		res.json({
+			unix: date.getTime(),
+			utc: date.toUTCString(),
+		});
 	} else {
-		date = new Date(input);
+		res.json({ error: 'Invalid Date' });
 	}
+});
 
-	res.json({
-		unix: date.getTime(),
-		utc: date.toUTCString(),
-	});
+app.get('/api/:date?', function (req, res) {
+	const { date } = req.params;
+
+	if (!date) {
+		res.json({
+			unix: new Date().getTime(),
+			utc: new Date().toUTCString(),
+		});
+	} else if (moment(date).isValid()) {
+		const dateObj = new Date(date);
+		res.json({
+			unix: dateObj.getTime(),
+			utc: dateObj.toUTCString(),
+		});
+	} else {
+		res.json({ error: 'Invalid Date' });
+	}
 });
 
 // listen for requests :)
